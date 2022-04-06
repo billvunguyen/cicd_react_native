052b5c786feebabf9710c9defe8260e4c6416746
Bill <billvunguyen@gmail.com>
2022-04-06 17:29:18 +0200

    update

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -10,5 +10,7 @@ import App from '../App';
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  const component = renderer.create(<App />);
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
